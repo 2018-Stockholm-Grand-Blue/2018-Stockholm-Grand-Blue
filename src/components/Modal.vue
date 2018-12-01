17c23b2cb3fce3a890c5970bd7679b810ba60dd5
femtioelva <naivigera@gmail.com>
2018-12-01 19:06:11 +0100

    edited modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,23 +2,38 @@
     <div class="modal-mask">
         <div class="modal-wrapper">
             <div class="modal-container">
+                <div class="headMenu">
+                    <i class="far fa-star fa-lg"></i> <i class="fas fa-times fa-lg" @click="$emit('close')"></i>
+                </div>
+
+                <div class="icons">
+                    <i class="fas fa-laptop fa-2x"></i>
+                </div>
+                <h4 class="title">
+                    Utvecklare
+                </h4>
+                <div class="employer">
+                   <i class="fas fa-globe"></i> ICA gruppen
+                </div>
+                <div class="location">
+                   <i class="fas fa-map-marked-alt"></i> Stockholm
+                </div>
 
                 <div class="modal-header">
-                    Header
+                    <i class="far fa-clock"></i> Heltid
                 </div>
 
                 <div class="modal-body">
                     <slot name="body">
-                        default body
+                       <h3>Beskrivning:</h3>  
+                       <p>Kunskaper:<br />- Python <br />- JavaScript <br />- Tidigare byggt RESTful HTTP API:s <br />- Kunskaper i NoSQL databaser. <br />- Van vidversionshanteringssystem som exempelvis git eller mercurial.</p>
                     </slot>
                 </div>
 
                 <div class="modal-footer">
                     <slot name="footer">
-                        default footer
-                        <button class="modal-default-button" @click="$emit('close')">
-                            OK
-                        </button>
+                        <p><strong>Kontakt:</strong> Per-Olof Vide, 08-01010110</p>
+                        <div class="myButton"><i class="far fa-check-circle fa-lg"></i> Ansök</div>
                     </slot>
                 </div>
             </div>
@@ -33,6 +48,8 @@
 </script>
 
 <style>
+
+
     .modal-mask {
         position: fixed;
         z-index: 9998;
@@ -43,6 +60,7 @@
         background-color: rgba(0, 0, 0, .5);
         display: table;
         transition: opacity .3s ease;
+        font: 'Muli', sans-serif;
     }
 
     .modal-wrapper {
@@ -58,7 +76,6 @@
         border-radius: 2px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
         transition: all .3s ease;
-        font-family: Helvetica, Arial, sans-serif;
     }
 
     .modal-header h3 {
@@ -86,4 +103,34 @@
         -webkit-transform: scale(1.1);
         transform: scale(1.1);
     }
+
+    .headMenu {
+        display: flex;
+        justify-content: space-between;
+
+    }
+
+.myButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #9acc85;
+    -webkit-box-shadow: inset 0px 1px 0px 0px #94de4e;
+    box-shadow: inset 0px 1px 0px 0px #c9f5b8;
+    background-color: #abff5c;
+    border: 1px solid #96d858;
+    display: inline-block;
+    cursor: pointer;
+    color: #0a0a0a;
+    font-size: 13px;
+    font-weight: bold;
+    padding: 6px 12px;
+    text-decoration: none;
+}
+.myButton:hover {
+    background-color: #bdfa85;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
 </style>
